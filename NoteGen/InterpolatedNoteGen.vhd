6ca63775ea53bbfad256b098bec12ff7bfb8bda0
fernandoka <fernandoka1997@gmail.com>
2019-12-17 22:27:43 +0100

    Update

--- NoteGen/InterpolatedNoteGen.vhd
+++ NoteGen/InterpolatedNoteGen.vhd
@@ -12,21 +12,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+
 entity InterpolatedNoteGen is
 generic (
-    WL 			: 	natural;
-	QM			:	natural;
-	START_ADDR	:	natural;
-    END_ADDR	:	natural;
-	TARGET_NOTE	:	real;
-	BASE_NOTE	:	real
+	START_ADDR	:	unsigned(25 downto 0);
+    END_ADDR	:	unsigned(25 downto 0);
+    TARGET_NOTE :   real;
+    BASE_NOTE   :   real
   );
   port(
     -- Host side
@@ -34,10 +33,13 @@ generic (
     clk                     : in    std_logic;  -- reloj del sistema
     cen_in                  : in    std_logic;   -- Activo a 1
     interpolateSampleRqt    : in    std_logic;
-    sample_out              : out    std_logic_vector(WL-1 downto 0);
+    sample_out              : out    std_logic_vector(15 downto 0);
+    
+    --Debug
+    ciOut                      :   out std_logic_vector(63 downto 0);
     
     --Mem side
-    samples_in              :   in  std_logic_vector(WL-1 downto 0);
+    samples_in              :   in  std_logic_vector(15 downto 0);
     memAck                  :   in std_logic;
     addr_out                :   out std_logic_vector(25 downto 0);
     readMem_out             :   out std_logic
@@ -51,81 +53,63 @@ use work.my_common.all;
 
 architecture Behavioral of InterpolatedNoteGen is
 ---------------------------	CONSTANTS --------------------------------------
-	constant QN				:	natural	:=	WL-QM;
-	constant Q_N_M_ARITH 	:	natural := 32;
+--	constant QN				:	natural	:=	WL-QM;
+--	constant Q_N_M_ARITH 	:	natural := 32;
 	
-	constant ZEROS 			:	signed(Q_N_M_ARITH-1 downto 0) := (others=>'0');
-	constant VALUE_TO_ROUND :	signed(Q_N_M_ARITH-1 downto 0) := to_signed(2**Q_N_M_ARITH,Q_N_M_ARITH);
-	constant MAX_VAL_SAMPLE	:	std_logic_vector(WL-2 downto 0) := (others=>'1');
+--	constant ZEROS 			:	signed(Q_N_M_ARITH-1 downto 0) := (others=>'0');
+	constant VALUE_TO_ROUND :	signed(50 downto 0) :="000" & X"000100000000"; --to_signed(2**Q_N_M_ARITH,Q_N_M_ARITH);
+--	constant MAX_VAL_SAMPLE	:	std_logic_vector(WL-2 downto 0) := (others=>'1');
 	
-	constant STEP_VAL		:	signed(2*Q_N_M_ARITH-1 downto 0) := toFix(TARGET_NOTE/BASE_NOTE,Q_N_M_ARITH,Q_N_M_ARITH);
-	
+	constant STEP_VAL_2		:	unsigned(63 downto 0) := toUnFix(TARGET_NOTE/BASE_NOTE,32,32);--X"000000010F38F350"; -- (TARGET_NOTE/BASE_NOTE)
+    constant STEP_VAL		:	unsigned(63 downto 0) := X"000000010F38F350"; -- (TARGET_NOTE/BASE_NOTE)
+
+	constant   MAX_POS_VAL     :   signed(16 downto 0) := "0" & X"7FFF";
+	constant   MAX_NEG_VAL     :   signed(16 downto 0) := "1" & X"0000";
 ---------------------------	SIGNALS	--------------------------------------
 	-- Registers
-	signal wtinI,wtinIPlus1 : signed(WL-1 downto 0);
+	signal wtinI,wtinIPlus1 : signed(15 downto 0);
 
 	
-	--signal	subVal							:	signed(QM+QN downto 0); -- 17 bits
-	--signal	addVal, roundVal, mulVal        :	signed(Q_N_M_ARITH+QM+QN+1 downto 0); -- 48 bits
-	--signal	finalVal						:	signed(QM+QN-1 downto 0);-- 16 bits
+	signal finalVal						:	signed(15 downto 0);-- 16 bits
+	signal subVal                       :   signed(16 downto 0);--17 bits
+	signal mulVal                       :   signed(49 downto 0); -- 50 bits
+    
+    signal roundVal                     :   signed(50 downto 0); -- 51 bits
+    signal addVal                       :   signed(16 downto 0);
 	
-	--signal	decimalPart						:	signed(Q_N_M_ARITH downto 0);-- 33 bits, msb de signo
-	--signal	ci								:	signed(2*Q_N_M_ARITH-1 downto 0);-- 64 bits	
-	
-	--Versión sudando de todo
-	signal	subVal							:	signed(QM+QN-1 downto 0); -- 16 bits
-	signal	addVal, roundVal, mulVal        :	signed(Q_N_M_ARITH+QM+QN-1 downto 0); -- 48 bits
-	signal	finalVal						:	signed(QM+QN-1 downto 0);-- 16 bits
-	
-	signal	decimalPart						:	signed(Q_N_M_ARITH downto 0);-- 33 bits, msb de signo
-	signal	ci								:	signed(2*Q_N_M_ARITH-1 downto 0);-- 64 bits	
+	signal decimalPart				    :	signed(32 downto 0);-- 33 bits, msb de signo
+	signal ci							:	unsigned(63 downto 0);-- 64 bits	
 	
 begin	
-		
---	Interpolation :
---		subVal <= (wtinIPlus1(WL-1) & wtinIPlus1) - (wtinI(WL-1) & wtinI); -- Q2.15 = Q1.15+Q1.15
---		
---		mulVal <= decimalPart*subVal; -- Q2.47 = Q2.15+Q0.32
---		
---		addVal <= mulVal + (wtinI(WL-1) & wtinI & ZEROS); -- Q2.47 = Q2.47+Q2.47( (Q2.15<<Q_N_M_ARITH)), Wrap here!!  
---	
---		roundVal <= (addVal + VALUE_TO_ROUND); -- Wrap here!!
---			
---		satur:
---			finalVal <= signed("0" & MAX_VAL_SAMPLE) when roundVal(Q_N_M_ARITH+QM+QN+1 downto Q_N_M_ARITH) > signed("000" & MAX_VAL_SAMPLE) else -- MAX_POS_VAL
---						signed("1" & not MAX_VAL_SAMPLE) when roundVal(Q_N_M_ARITH+QM+QN+1 downto Q_N_M_ARITH) < signed("111" & not MAX_VAL_SAMPLE) else -- MAX_NEG_VAL
---						roundVal(Q_N_M_ARITH+QM+QN-1 downto Q_N_M_ARITH);
---						
---						
---		decimalPart	<= "0" & ci(Q_N_M_ARITH-1 downto 0); -- Siempre Positivo
 
--- Otro intento, sudando de todo
-	Interpolation :
-		subVal <= wtinIPlus1 - wtinI; -- Wrap here!!
-		
-		mulVal <= decimalPart*subVal;
-		
-		addVal <= mulVal + signed(wtinI & ZEROS);  -- Wrap here!!
-	
-		roundVal <= (addVal + VALUE_TO_ROUND); -- Wrap here!!
-			
-		finalVal <= roundVal(Q_N_M_ARITH+QM+QN-1 downto Q_N_M_ARITH); -- Wrap here!!
-						
-						
-		decimalPart	<= signed("0" & ci(Q_N_M_ARITH-1 downto 0)); -- Siempre Positivo
+Interpolate:
+    subVal <= (wtinIPlus1(15) & wtinIPlus1) - (wtinI(15) & wtinI); -- Q17.0 = Q16.0-Q16.0
+    
+    mulVal <= decimalPart*subVal; -- Q49.0 = Q17.0*Q33.0
+    
+    roundVal <= (mulVal(49) & mulVal) + VALUE_TO_ROUND; --Q50.0 = Q49.0+Q49.0
+    
+    addVal <= roundVal(50 downto 34) + (wtinI(15) & wtinI); --Q17.0 = Q16.0+Q16.0
+    
+    finalVal <= MAX_POS_VAL(15 downto 0) when addVal > MAX_POS_VAL else
+                MAX_NEG_VAL(15 downto 0) when addVal < MAX_NEG_VAL else
+                addVal(15 downto 0);
 
+
+    decimalPart <= signed("0" & ci(31 downto 0));
 	
+ciOut <= std_logic_vector(STEP_VAL_2);
+
 	filterRegisters :
-  process (rst_n, clk)
+  process (rst_n, clk,memAck,cen_in,interpolateSampleRqt)
       type states is (idle, getSample1, getSample2, interpolate, calculateNextStep); 
       variable state: states;
       variable cntr : natural range 0 to 1;
-      variable currentAddr : natural range START_ADDR to END_ADDR;
-      variable wtout : signed(QM+QN-1 downto 0);
+      variable currentAddr : unsigned(25 downto 0);
+      variable wtout : signed(15 downto 0);
   begin  
     
-    addr_out <= (others=>'0');
-    addr_out <= std_logic_vector(to_unsigned(currentAddr,26));
+    addr_out <= std_logic_vector(currentAddr);
     sample_out <= std_logic_vector(wtout);
 
     if rst_n='0' then
@@ -182,7 +166,7 @@ begin
                         ci	  <= ci+STEP_VAL; -- Calculate next step
                     elsif cntr=1 and interpolateSampleRqt ='1' then
                         wtout := finalVal;
-                        currentAddr := START_ADDR+to_integer(ci(2*Q_N_M_ARITH-1 downto Q_N_M_ARITH)); -- Calculate next addr
+                        currentAddr := START_ADDR+unsigned(ci(57 downto 32)); -- Calculate next addr
                         state := calculateNextStep;
                     end if;
                     
